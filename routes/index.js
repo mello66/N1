54d379b0c4362e40f95741d1d3d1919a9dfc640d
wenhanzhang <alemonpen@gmail.com>
2019-09-30 22:43:15 -0700

    test1

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'cs651' });
+});
+
+module.exports = router;
