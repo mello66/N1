954cbd57c32de35deb54aa0641d264c81494a2c7
wenhanzhang <alemonpen@gmail.com>
2019-09-30 23:12:26 -0700

    test1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'cs651' });
+  res.render('index', { title: 'ExerciseN1' });
 });
 
 module.exports = router;
